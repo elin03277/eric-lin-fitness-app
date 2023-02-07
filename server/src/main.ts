8f649c44f89114ca64557fc5180f4b2995f8fc30
Eric Lin <elin03277@gmail.com>
2023-02-07 15:56:31 -0800

    add cors and helmut to resolve cors policy issues

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+  app.enableCors();
 
   app.useGlobalPipes(new ValidationPipe());
 
